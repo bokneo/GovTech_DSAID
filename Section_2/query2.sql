a2bfdd1e07cf556f6d7110b62c2c24fa07ba99a3
bokneo <69624094+bokneo@users.noreply.github.com>
2021-06-20 16:39:03 +0800

    Update query2.sql

--- Section_2/query2.sql
+++ Section_2/query2.sql
@@ -1,9 +1,9 @@
 /* the top 3 car manufacturers that customers bought by sales (quantity) and
 the sales number for it in the current month. */
 
-SELECT t.customer_name, SUM(c.price)
-FROM transaction AS t, car AS c
-WHERE t.salesperson = c.salesperson
-AND t.serialnumber = c.serialnumber
-AND t.manufacturers = c.manufacturers
-GROUP BY t.customer_name
+SELECT t.manufacturers, count(*) as sales
+FROM transaction AS t
+WHERE date_sold >= date_trunc('month', CURRENT_DATE)
+GROUP BY t.manufacturers
+ORDER BY sales DESC
+LIMIT 3
