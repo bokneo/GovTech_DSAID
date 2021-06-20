5aa0c28c0675ef806a163b1a782282356f3aba7c
bokneo <69624094+bokneo@users.noreply.github.com>
2021-06-20 16:28:00 +0800

    Update query1.sql

--- Section_2/query1.sql
+++ Section_2/query1.sql
@@ -3,4 +3,6 @@
 SELECT t.customer_name, SUM(c.price)
 FROM transaction AS t, car AS c
 WHERE t.salesperson = c.salesperson
+AND t.serialnumber = c.serialnumber
+AND t.manufacturers = c.manufacturers
 GROUP BY t.customer_name
