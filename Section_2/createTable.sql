46e0dd0530b9d4ac8a235ef99f4da130888cdc59
bokneo <69624094+bokneo@users.noreply.github.com>
2021-06-20 15:48:31 +0800

    Updated DDL for Transactions

--- Section_2/createTable.sql
+++ Section_2/createTable.sql
@@ -1,20 +1,19 @@
 CREATE TABLE car(
-    id VARCHAR(128) NOT NULL,
-    salesperson VARCHAR(128) NOT NULL,
+    salesperson VARCHAR(128) NOT NULL UNIQUE,
     manufacturers VARCHAR(128) NOT NULL,
     modelname VARCHAR(255) NOT NULL,
     serialnumber VARCHAR(255) NOT NULL,
     weight NUMERIC(12,2) NOT NULL,
     price NUMERIC(12,2) NOT NULL,
-    PRIMARY KEY (id, username, name)
+    PRIMARY KEY (serialnumber, manufacturers)
     FOREIGN KEY(id, username) REFERENCES auth_user(id, username)
 );
 
 CREATE TABLE transaction(
     id SERIAL PRIMARY KEY,
     customer_name VARCHAR(255),
-    user VARCHAR(128) REFERENCES users(id),
+    customer_phone VARCHAR(255),
     salesperson VARCHAR(128),
-    carcharacteristics TEXT,
-    FOREIGN KEY(project_name) REFERENCES project(project_name)
+    car_characteristics TEXT,
+    FOREIGN KEY(salesperson) REFERENCES car(salesperson)
 );
