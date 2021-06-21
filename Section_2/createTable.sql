e3204af6c99a4e67c2bfb299eb1a9fe73c4d0506
bokneo <69624094+bokneo@users.noreply.github.com>
2021-06-21 15:55:17 +0800

    Update createTable.sql

--- Section_2/createTable.sql
+++ Section_2/createTable.sql
@@ -1,20 +1,31 @@
-CREATE TABLE car(
-    salesperson VARCHAR(128) NOT NULL,
-    manufacturers VARCHAR(128) NOT NULL,
-    modelname VARCHAR(128) NOT NULL,
-    serialnumber VARCHAR(128) NOT NULL,
-    weight NUMERIC(12,2) NOT NULL,
-    price NUMERIC(12,2) NOT NULL,
-    PRIMARY KEY (salesperson, serialnumber, manufacturers)
+CREATE TABLE Salesperson(
+    id SERIAL PRIMARY KEY,
+    last_name VARCHAR(128),
+    first_name VARCHAR(128)
 );
 
-CREATE TABLE transaction(
-    id SERIAL PRIMARY KEY,
-    customer_name VARCHAR(128),
-    customer_phone VARCHAR(128),
-    salesperson VARCHAR(128),
+CREATE TABLE Car(
+    serialnumber VARCHAR(128) NOT NULL PRIMARY KEY,
     manufacturers VARCHAR(128) NOT NULL,
-    serialnumber VARCHAR(128) NOT NULL,
-    date_sold TIMESTAMP default NULL,
-    FOREIGN KEY(salesperson, serialnumber, manufacturers) REFERENCES car(salesperson, serialnumber, manufacturers)
+    modelname VARCHAR(128) NOT NULL,
+    weight NUMERIC(12,2) NOT NULL,
+    price NUMERIC(12,2) NOT NULL
 );
+
+CREATE TABLE Customer(
+    id SERIAL PRIMARY KEY,
+    last_name VARCHAR(128),
+    first_name VARCHAR(128),
+    customer_phone VARCHAR(128),
+    date_bought TIMESTAMP default NULL
+);
+
+
+CREATE TABLE Transaction(
+    invoice_id SERIAL PRIMARY KEY,
+    serialnumber VARCHAR(128) references Car(serialnumber),
+    customer_id INTEGER references Customer(id),
+    salesperson_id INTEGER,
+    FOREIGN KEY(salesperson_id) REFERENCES Salesperson(id)
+);
+
