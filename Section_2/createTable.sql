8725b8ad4e22dd7434afa97a7a0106c3f592c7fd
bokneo <69624094+bokneo@users.noreply.github.com>
2021-06-21 13:06:59 +0800

    Update createTable.sql

--- Section_2/createTable.sql
+++ Section_2/createTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE car(
     salesperson VARCHAR(128) NOT NULL,
     manufacturers VARCHAR(128) NOT NULL,
-    modelname VARCHAR(255) NOT NULL,
-    serialnumber VARCHAR(255) NOT NULL,
+    modelname VARCHAR(128) NOT NULL,
+    serialnumber VARCHAR(128) NOT NULL,
     weight NUMERIC(12,2) NOT NULL,
     price NUMERIC(12,2) NOT NULL,
     PRIMARY KEY (salesperson, serialnumber, manufacturers)
@@ -10,11 +10,11 @@ CREATE TABLE car(
 
 CREATE TABLE transaction(
     id SERIAL PRIMARY KEY,
-    customer_name VARCHAR(255),
-    customer_phone VARCHAR(255),
+    customer_name VARCHAR(128),
+    customer_phone VARCHAR(128),
     salesperson VARCHAR(128),
     manufacturers VARCHAR(128) NOT NULL,
-    serialnumber VARCHAR(255) NOT NULL,
+    serialnumber VARCHAR(128) NOT NULL,
     date_sold TIMESTAMP default NULL,
     FOREIGN KEY(salesperson, serialnumber, manufacturers) REFERENCES car(salesperson, serialnumber, manufacturers)
 );
